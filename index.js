da90485bb8e316d706c3ce6d8acd4d2c391d7f20
janerishbaluca <janerishbaluca@gmail.com>
2018-10-14 23:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll(' .ranked-list');
+  for (let i=0;i<rankedLists.length;i++) {
+    for (let j=0;j<rankedLists[i].length;j++) {
+      rankedLists[i].children[j].innerHTML = parseInt(rankedLists[i].children[j].innerHTML) + n;
+    }
+  }
+  return rankedLists
+}
+
+function deepestChild() {
+  var nodes = document.querySelectorAll('#grand-node div');
+  return nodes[nodes.length- 1];
+}
